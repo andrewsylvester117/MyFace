06071634874f797017dfab726e6b689175107a73
Robert Walker <Banana0042@gmail.com>
2017-11-07 15:18:10 -0700

    Created Project

---
+++ MyFace/MyFace/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFace.Startup))]
+namespace MyFace
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
