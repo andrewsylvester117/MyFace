111a4a914259c5d30cca14160e14af06bfc125ca
Robert Walker <Banana0042@gmail.com>
2017-11-08 16:07:53 -0700

    Added Post object - includes byte array for an image - includes string for text - includes id

---
+++ MyFace/MyFace/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFace.Models
+{
+	public class Post
+	{
+		public int id { get; set; }
+		public string textcontent { get; set; }
+		public byte[] imagecontent { get; set; }
+
+	}
+}
