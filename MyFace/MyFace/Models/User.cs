30cb31e25c1e8814bad223ea071f162ebfffd1fd
Robert Walker <Banana0042@gmail.com>
2017-11-09 14:58:05 -0700

    Added Post Form and display partial views Started on User class

--- MyFace/MyFace/Models/User.cs
+++ MyFace/MyFace/Models/User.cs
@@ -5,11 +5,11 @@ using System.Web;
 
 namespace MyFace.Models
 {
-	public class Post
+	public class User
 	{
 		public int id { get; set; }
-		public string textcontent { get; set; }
-		public string imagepath { get; set; }
+		public string name { get; set; }
+
 
 	}
 }
