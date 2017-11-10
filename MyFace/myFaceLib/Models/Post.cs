24623ede2719cab5b7d353ba67fc13e96cb888d0
Robert Walker <Banana0042@gmail.com>
2017-11-10 15:57:11 -0700

    Shuffled Models to myFaceLib Project

---
+++ MyFace/myFaceLib/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace myFaceLib.Models
+{
+	public class Post
+	{
+		public int publisherid { get; set; }
+		public int id { get; set; }
+		public int parentid { get; set; }
+		public string postheader {get; set;}
+		public string textcontent { get; set; }
+		public Image imagecontent { get; set; }
+
+		public int likecount { get; set; }
+		public int dislikecount { get; set; }
+
+	}
+}
