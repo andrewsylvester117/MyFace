e83158a17b8df33f43ea25984425cac9f4bfbe68
Andrew <andrewsylvester117@gmail.com>
2017-11-17 14:47:26 -0700

    Stuff

---
+++ MyFace/myFaceLib/Services/MyFaceServicable.cs
@@ -0,0 +1,17 @@
+﻿using myFaceLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFaceLib.Services
+{
+	interface MyFaceServicable
+	{
+		List<FriendList> GetAllFriends(User user);
+		User GetUserByID(int id);
+		Post GetPostByID(int id);
+
+	}
+}
---
+++ MyFace/myFaceLib/Services/MyFaceService.cs
@@ -0,0 +1,37 @@
+﻿using myFaceLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFaceLib.Services
+{
+	class MyFaceService : MyFaceServicable
+	{
+		public List<FriendList> GetAllFriends(User user)
+		{
+			List<FriendList> usersFriends = new List<FriendList>();
+
+			// Need to add refrence to myFaceDal, but complains about a circular dependency when doing so.
+
+			//using ()
+			//{
+
+			//}
+				return usersFriends;
+		}
+
+		public User GetUserByID(int id)
+		{
+
+			return new User();
+		}
+
+		public Post GetPostByID(int id)
+		{
+
+			return new Post();
+		}
+	}
+}
